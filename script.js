a9fb4a0a09125c8292799bc134596f2abee0fa7d
RomanCherkunov <roman4erkunov@mail.ru>
2026-04-23 15:50:41 +0300

    create math functions and check it

---
+++ script.js
@@ -0,0 +1,20 @@
+function sum (a, b) {
+    return a + b
+}
+
+function subtract (a, b) {
+    return a - b 
+}
+
+function multiply (a, b) {
+    return a * b
+}
+
+function divide (a, b) {
+    return a / b
+}
+
+console.log(sum(6, 3))
+console.log(subtract(6, 3))
+console.log(multiply(6, 3))
+console.log(divide(6, 3))
